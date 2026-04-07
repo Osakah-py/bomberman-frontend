b703b098ac59b6c4e1e188e1e3752b3f3ba09148
alex <lescot.alexg@gmail.com>
2026-04-07 15:40:10 +0200

    bordures

--- src/Game/components/Box.jsx
+++ src/Game/components/Box.jsx
@@ -7,19 +7,17 @@ extend({ Graphics, Text, Container });
 
 const Box = ({x, y}) => {
   const [rotation, setRotation] = useState(0);
-  const [moveRight, setMoveRight] = useState(0);
   const i = useRef(0);
 
   useTick(ticker => {
      
     i.current += 0.05 * ticker.deltaTime;
     setRotation(-10 + Math.sin(i.current / 10 + Math.PI * 2) * 10);
-    setMoveRight(i.current % 90);
   });
   
 return (
   <pixiContainer>
-    <pixiText text={`rotation: ${rotation.toFixed(2)} position: ${moveRight.toFixed(2)}`} x={50} y={50} anchor={0.5} />
+    <pixiText text={`rotation: ${rotation.toFixed(2)} position: x :${x.toFixed(2)} y ${y.toFixed(2)}`} x={300} y={50} anchor={0.5} />
     <pixiGraphics
       draw={(g) => {
         g.clear();
--- src/Game/system/usePhysics.js
+++ src/Game/system/usePhysics.js
@@ -1,5 +1,6 @@
 import { useTick } from "@pixi/react";
 import { useState, useRef } from "react"
+import { APP_HEIGHT, APP_WIDTH } from "../../constants";
 
 export const usePhysics = (keys) => {
     const [x, setX] = useState(300);
@@ -9,8 +10,9 @@ export const usePhysics = (keys) => {
     useTick ((ticker) => {
         const speed = 3;
         const friction = 0.9;
+        const bounce = 2;
 
-        if (keys.current['ArrowLeft']) {console.log("arrowleft"); velocity.current.x -= speed;}
+        if (keys.current['ArrowLeft']) velocity.current.x -= speed;
         if (keys.current['ArrowRight']) velocity.current.x += speed;
         if (keys.current['ArrowUp']) velocity.current.y -= speed;
         if (keys.current['ArrowDown']) velocity.current.y += speed;
@@ -18,8 +20,28 @@ export const usePhysics = (keys) => {
         velocity.current.x *= friction;
         velocity.current.y *= friction;
 
-        setX(prev => prev + velocity.current.x * ticker.deltaTime);
-        setY(prev => prev + velocity.current.y * ticker.deltaTime);
+        let newX = x + velocity.current.x * ticker.deltaTime
+        let newY = y + velocity.current.y * ticker.deltaTime
+
+        if (newX < 0) {
+            newX = 0;
+            velocity.current.x = Math.abs(velocity.current.x) * bounce
+        }
+        if (newX > APP_WIDTH) {
+            newX = APP_WIDTH;
+            velocity.current.x = -Math.abs(velocity.current.x) * bounce
+        }
+        if (newY < 0) {
+            newY = 0;
+            velocity.current.y = Math.abs(velocity.current.y) * bounce
+        }
+        if (newY > APP_HEIGHT) {
+            newY = APP_HEIGHT;
+            velocity.current.y = -Math.abs(velocity.current.y) * bounce
+        }
+
+        setX(newX);
+        setY(newY);
     });
 
     return {x, y};
