e526ee4190b66df8efabbdf4a0064f894c628327
Draou <imannorr.draou@etu.inp-n7.fr>
2026-05-19 15:32:10 +0200

    menu presque bo

--- src/menu.jsx
+++ src/menu.jsx
@@ -1,16 +1,19 @@
 import { useState } from "react";
+import "./menu.css";
 
 
 export default function Menu({ setPage }) {
     const utilisateur = localStorage.getItem("user");
      return (
-    <div>
-      <h1>Menu</h1>
+    <div className="menu-container">
+      <h1>BONBERMAN</h1>
+      <h2>Menu</h2>
       <div>
         {/*<button onClick={() => setPage("game")}> Partie </button>*/}
-        <button onClick={() => {localStorage.removeItem("user"); setPage("login");}}> Déconnexion </button>
-        <button onClick={() => setPage("amis")}> Mes Amis </button>
         <button onClick={() => setPage("partie")}> Rejoindre/Créer une partie ! </button>
+        <button onClick={() => setPage("amis")}> Mes Amis </button>
+                <button onClick={() => {localStorage.removeItem("user"); setPage("login");}}> Déconnexion </button>
+
 
       </div>
     </div>
