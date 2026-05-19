816b668fe85d32a4f4ab20b2a972c4f59c55b174
alex <lescot.alexg@gmail.com>
2026-05-19 21:52:55 +0200

    new ip

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,5 @@ export const APP_HEIGHT = 600;
 export const MAP_WIDTH = 2400;
 export const MAP_HEIGHT = 1800;
 
-export const BACKEND_URL = "http://192.168.14.161:8080";
-export const BACKEND_SOCKET = "ws://192.168.14.161:8080";
+export const BACKEND_URL = "http://10.177.106.32:8080";
+export const BACKEND_SOCKET = "ws://10.177.106.32:8080";
