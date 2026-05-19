5900851ebf98c8fa4d4be461106e615768e800c9
alex <lescot.alexg@gmail.com>
2026-05-19 18:51:12 +0200

    amis back again

--- src/amis.jsx
+++ src/amis.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useEffect } from "react";
+import { BACKEND_URL } from './constants'
 import axios from "axios";
 
 export default function Signup({ setPage }) {
