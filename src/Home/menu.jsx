2b2678e8f75453173b0e302444d26d418a9f2b87
Draou <imannorr.draou@etu.inp-n7.fr>
2026-05-20 12:01:22 +0200

    suppression de compte

--- src/Home/menu.jsx
+++ src/Home/menu.jsx
@@ -1,19 +1,61 @@
 import { useState } from "react";
 import "./menu.css";
+import { BACKEND_URL } from '../constants'
+import axios from "axios";
+
 
 
 export default function Menu({ setPage }) {
     const utilisateur = localStorage.getItem("user");
+    const [confirmation, setConfirmation] = useState(false);
+    const [mdp, setMdp] = useState("");
+
+    const executerSuppression = () => {
+        const params = new URLSearchParams();
+        params.append("pseudo", utilisateur);
+        params.append("password", mdp); 
+        axios.post(BACKEND_URL + '/api/supprimerCompte', params)
+        .then((res) => {
+            alert("Compte supprimé.");
+            localStorage.clear();
+            setPage("login");
+        })
+        .catch((err) => {
+            alert("Erreur avec.");
+        });
+    };
+
      return (
     <div className="menu-container">
-      <h1>BONBERMAN</h1>
+      <h1>BOMBERMAN</h1>
       <h2>Menu</h2>
-      <div>
-        {/*<button onClick={() => setPage("game")}> Partie </button>*/}
-        <button onClick={() => setPage("partie")}> Rejoindre/Créer une partie ! </button>
-        <button onClick={() => setPage("amis")}> Mes Amis </button>
-        <button onClick={() => {localStorage.removeItem("user"); setPage("login");}}> Déconnexion </button>
-      </div>
-    </div>
-     );
+      {!confirmation ? ( //la on a lu truc normal
+                <div>
+                    <button onClick={() => setPage("partie")}> Rejoindre/Créer une partie ! </button>
+                    <button onClick={() => setPage("amis")}> Mes Amis </button>
+                    <button onClick={() => {localStorage.removeItem("user"); setPage("login");}}> Déconnexion </button>
+                    <button onClick={() => setConfirmation(true)} style={{ backgroundColor: "#d9534f", color: "white", marginTop: "20px" }}> 
+                        Supprimer le compte 
+                    </button>
+                </div>
+            ) : ( // so on a appuyé sur supperession de compte
+                <div style={{ backgroundColor: "#f8d7da", padding: "15px", borderRadius: "8px", marginTop: "10px" }}>
+                    <p>Entrez votre mot de passer :</p>
+                    <input 
+                        type="password" 
+                        value={mdp} 
+                        onChange={(e) => setMdp(e.target.value)}
+                        placeholder="Mot de passe"
+                    />
+                    <br />
+                    <button onClick={executerSuppression}>
+                        Confirmer la suppression
+                    </button>
+                    <button onClick={() => setConfirmation(false)}>
+                        Annuler
+                    </button>
+                </div>
+            )}
+        </div>
+    );
 } 
