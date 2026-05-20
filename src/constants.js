3b1c46542afa5ed3f3855ebff40f639a2dbd17b4
alex <lescot.alexg@gmail.com>
2026-05-20 11:15:30 +0200

    ip update

--- src/constants.js
+++ src/constants.js
@@ -4,5 +4,5 @@ export const APP_HEIGHT = 600;
 export const MAP_WIDTH = 650;
 export const MAP_HEIGHT = 650;
 
-export const BACKEND_URL = "http://192.168.2.4:8080";
-export const BACKEND_SOCKET = "ws://192.168.2.4:8080";
+export const BACKEND_URL = "http://172.22.221.215:8080";
+export const BACKEND_SOCKET = "ws://172.22.221.215:8080";
